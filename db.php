36b3a5d66b6814f815d65915a803c6ba09d07931
shobanaivaraja <36257085+shobanaivaraja@users.noreply.github.com>
2018-02-23 22:34:05 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost:3306'); 
+ define('DB_NAME', 'hackathon'); 
+ define('DB_USER','root'); 
+ define('DB_PASSWORD',''); 
+ $con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) ;
+ ?>
